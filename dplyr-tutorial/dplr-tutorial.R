84bf38b4e0cd8481b2cf445ada460beb6929b048
Robert Jonczy <robert.jonczy@gmail.com>
2015-05-28 18:58:24 +0200

    1st part of dplyr tutorial

--- dplyr-tutorial/dplr-tutorial.R
+++ dplyr-tutorial/dplr-tutorial.R
@@ -1,3 +1,5 @@
+# My practise of dplyr based on https://rpubs.com/justmarkham/dplyr-tutorial
+
 library(dplyr)
 library(hflights)
 
@@ -6,3 +8,21 @@ data()
 
 # load dataset
 data(hflights)
+
+head(hflights)
+
+class(hflights) # check class of hflights
+
+# convert to local data frame
+flights <- tbl_df(hflights)
+class(flights) # check class of flights
+
+# prints only 10 rows of local data frame
+flights
+
+print(flights, n = 20)
+
+# convert to normal data frame
+data.frame(head(flights))
+
+
